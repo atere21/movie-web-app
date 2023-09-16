9720fd45ae7f696ca14ec5f1c2fa57ccdb5f576a
atere21 <you@tosinateret@gmail.com>
2023-09-16 14:07:34 -0700

    slice

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -8,10 +8,9 @@ import Movie from './Movie';
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
   const slideRight = () => {
-    var slider = document.getElementById('slider')
+    var slider = document.getElementById('slider');
     slider.scrollRight = slider.scrollRight = 500;
-  }
-  
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,31 +27,27 @@ const Row = ({ title, fetchURL }) => {
 
   console.log(movies);
 
+  // Limit the number of movies to 10
+  const displayedMovies = movies.slice(0, 10);
+
   return (
     <div className="my-6 bg-white w-full">
       <div className="flex justify-between mx-4">
         <h2 className="text-black font-bold md:text-2xl text-xl p-4">{title}</h2>
         <div className="inline-flex text-red-800 font-semibold cursor-pointer my-[14px] ">
-          See More 
-          <AiOutlineRight  onClick ={slideRight}
-          className=" w-4 my-[0.3em] ml-4" />
+          See More
+          <AiOutlineRight onClick={slideRight} className=" w-4 my-[0.3em] ml-4" />
         </div>
       </div>
-      
-        {/* <AiOutlineRight onClick ={slideRight}
-        className =" bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer"
-        size ={40}
-        /> */}
 
-        <div
-          id={'slider'}
-          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 w-full h-full gap-4 overflow-x-scroll whitespace-nowrap relative"
-          style={{ overflowY: 'hidden', scrollbarWidth: 'none' }} // Hide vertical scrollbar
-        >
-          {movies.map((item) => (
-            <Movie key={item.id} item={item} />
-          ))}
-      
+      <div
+        id={'slider'}
+        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 w-full h-full gap-4 overflow-x-scroll whitespace-nowrap relative"
+        style={{ overflowY: 'hidden', scrollbarWidth: 'none' }} // Hide vertical scrollbar
+      >
+        {displayedMovies.map((item) => (
+          <Movie key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
