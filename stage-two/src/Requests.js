30fed02e399f7d853c6b4e5a05320383d0430137
atere21 <you@tosinateret@gmail.com>
2023-09-16 01:01:02 -0700

    fixed

--- stage-two/src/Requests.js
+++ stage-two/src/Requests.js
@@ -2,10 +2,10 @@ const key = 'c956c6ae0469537b152ae95ec0bad867';
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
- 
-  requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}&language=en-US&page=1`, // Corrected URL for trending movies
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
   requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
   requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 };
 
-export default requests;
+export default requests
